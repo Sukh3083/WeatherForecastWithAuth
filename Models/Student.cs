26c25911b9232338edb6fb87fed76306bdb33f1f
Sukhjinder Singh <200491857@student.georgianc.on.ca>
2021-12-14 10:40:47 -0500

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherForecastWithAuth.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+
+    }
+}
---
+++ Services/IAuthenticateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecastWithAuth.Models;
+
+namespace WeatherForecastWithAuth.Services
+{
+    public interface IAuthenticateService
+    {
+        Student Authenticate(String UserName, String Password);
+
+    }
+}
